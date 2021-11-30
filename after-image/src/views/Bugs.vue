58bfe36cf43dd3dd33c361b74b8e47bf17e33ccb
Derek <shouldergiant@icloud.com>
2021-11-29 22:53:19 -0500

    Broad strokes concept

--- after-image/src/views/Bugs.vue
+++ after-image/src/views/Bugs.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="bugs">
     <h1>There are bugs here</h1>
+    <div><p>Report a bug</p></div>
+    <div><p>Report a fix</p></div>
+    <div><p>Metrics</p></div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Bugs",
+  components: {
+  },
+};
+</script>
--- after-image/src/views/Home.vue
+++ after-image/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h1>I HAVE THE PANTS</h1>
+    <div><p>Mantras</p></div>
+    <div><p>Bugs</p></div>
+    <div><p>Intentions</p></div>
   </div>
 </template>
 
