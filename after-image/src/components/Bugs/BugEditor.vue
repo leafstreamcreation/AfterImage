47af6a3e95f098bf1ba7684478c246d002c00d5b
Derek <shouldergiant@icloud.com>
2021-12-22 15:54:20 -0500

    Bug filter system for ease of use

--- after-image/src/components/Bugs/BugEditor.vue
+++ after-image/src/components/Bugs/BugEditor.vue
@@ -2,12 +2,30 @@
   <div class="bug-editor">
     <form @submit.prevent="">
       <div>
+        <button
+          v-for="title in titleMatches"
+          :key="title"
+          @click="setTitle(title)"
+        >
+          {{ title }}
+        </button>
+      </div>
+      <div>
         <label for="new-bug-title">Title: </label>
         <input type="text" id="new-bug-title" v-model="state.title" />
       </div>
       <div>
         <label for="new-bug-details">Details: </label>
         <input type="text" id="new-bug-details" v-model="state.details" />
+      </div>
+      <div>
+        <button
+          v-for="condition in conditionMatches"
+          :key="condition"
+          @click="setCondition(condition)"
+        >
+          {{ condition }}
+        </button>
       </div>
       <div>
         <label for="new-bug-condition">Condition: </label>
@@ -40,11 +58,48 @@ export default {
       newCondition: "",
     };
   },
+  computed: {
+    bugs() {
+      return this.$store.state.bugs;
+    },
+    titleMatches() {
+      const title = this.state.title.toLowerCase();
+      const matches =
+        title === ""
+          ? []
+          : this.bugs
+              .map((bug) => bug.title)
+              .filter((bugTitle) => bugTitle.includes(title));
+      return matches[0] === title ? [] : [...new Set(matches)];
+    },
+    conditionsByTitle() {
+      const title = this.state.title.toLowerCase();
+      return [
+        ...new Set(
+          this.bugs
+            .filter((bug) => bug.title === title)
+            .reduce(
+              (allConditions, bug) => [...allConditions, ...bug.conditions],
+              []
+            )
+        ),
+      ];
+    },
+    conditionMatches() {
+      const condition = this.newCondition.toLowerCase();
+      const matches = this.conditionsByTitle.filter((titleCondition) =>
+        titleCondition.includes(condition)
+      );
+      return matches[0] === condition ? [] : matches;
+    },
+  },
   methods: {
     finishEdit() {
       if (this.state.title === "") {
         //set style for invalid empty string
       } else {
+        this.state.title = this.state.title.toLowerCase();
+        this.state.conditions = [...new Set(this.state.conditions)];
         this.$store.dispatch("newBug", { bug: this.state });
         this.$emit("finishEdit", this.state);
         this.clear();
@@ -55,9 +110,16 @@ export default {
       if (this.newCondition === "") {
         //set style for invalid empty string
       } else {
-        this.state.conditions.push(this.newCondition);
+        this.state.conditions.push(this.newCondition.toLowerCase());
         this.newCondition = "";
       }
+    },
+    setCondition(condition) {
+      this.newCondition = condition;
+      this.addCondition();
+    },
+    setTitle(title) {
+      this.state.title = title;
     },
     clear() {
       this.state = {
